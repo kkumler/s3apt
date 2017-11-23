b6b05074c5485fa52235750f10eda3250605c983
Iain Patterson <iain.patterson@hivehome.com>
2017-11-23 16:34:00 +0000

    Deploy to pt2-isengard bucket.

---
+++ package/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+version=$1; shift
+s3=$1; shift
+if [ -z "$version" ]; then
+  echo >&2 "Usage: $0 <version> [<bucket>[/<prefix>]]"
+  exit 1
+fi
+
+  set -e
+
+ROOT=$(mktemp -d --tmpdir=$PWD)
+trap 'rm -rf "$ROOT"' ERR EXIT
+virtualenv "$ROOT"
+. "$ROOT/bin/activate"
+pip install -r requirements.txt
+
+zip="isengard-$version.zip"
+scratch="$ROOT/$zip"
+zip "$scratch" s3apt.py config.py
+( cd "$ROOT/lib/python2.7/site-packages" && exec zip -r "$scratch" * )
+mv -f -v "$scratch" "$zip"
+
+[ -n "$s3" ] && aws s3 cp "$zip" "s3://${s3#s3\://}"
